cc56785224b81b9acf45f3266b8e216b5d60f662
admin <admin@Macbook-Pro.local>
2018-04-03 08:14:09 +0300

    added to te js

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+var theSeries = [];
+
+function pingpong(num) {
+  for (var i = 1; i <= num; i++) {
+    if (i % 15===0) {
+      theSeries.push('pingpong');
+    } else if (i % 5 === 0) {
+      theSeries.push('pong');
+    } else if (i % 3 === 0) {
+      theSeries.push('ping');
+    } else {
+      theSeries.push(i);
+    }
+  };
+};
+$(document).ready(function() {
+      $("#pingpong").submit(function(event) {
+          event.preventDefault();
+          var result = parseInt($("#inputNumber").val())
+          pingpong(result);
+          // console.log(theSeries);
+          theSeries.forEach(function(e) {
+            // console.log(theSeries);
+            $("#result").append($("<li>").html(e));
+          })
+        });
+      });
